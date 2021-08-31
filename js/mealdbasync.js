39e2b5c2a336b92fca4259f66de364f8bbc8db1a
mahbubmanna <mahbubmanna@yahoo.com>
2021-08-31 07:39:40 +0600

    first commit

---
+++ js/mealdbasync.js
@@ -0,0 +1,112 @@
+const searchFood = async () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+
+
+    // clear single meal-detail (if any)
+    const mealDetails = document.getElementById('meal-details');
+    mealDetails.innerHTML = ``;
+
+    if (searchField.value == '') {
+
+        const searchResult = document.getElementById('search-result');
+        // to remove previous result
+        // searchResult.innerHTML = '';
+        searchResult.textContent = '';
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `<h5>Please, write something to search.</h5>`;
+        searchResult.appendChild(div);
+    }
+    else {
+        const searchResult = document.getElementById('search-result');
+        searchResult.innerHTML = `
+        <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+        `
+        // clear data
+        searchField.value = '';
+        // load data
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+        // console.log(url);
+
+
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            displaySearchResult(data.meals)
+        }
+        catch (error) {
+            console.log(error);
+        }
+        // .catch(error => console.log(error));
+
+        /* fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.meals)) */
+    }
+}
+
+const displaySearchResult = meals => {
+    const searchResult = document.getElementById('search-result');
+    // to remove previous result
+    // searchResult.innerHTML = '';
+    searchResult.textContent = '';
+    if (meals == null) {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `<h5>No result found</h5>`;
+        searchResult.appendChild(div);
+    }
+    else {
+        meals.forEach(meal => {
+            console.log(meal);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">
+                    ${meal.strInstructions.slice(0, 200)}
+                    </p>
+                </div>
+            </div>
+            `
+            searchResult.appendChild(div);
+        })
+    }
+
+}
+
+const loadMealDetail = async mealId => {
+    console.log(mealId);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+    displayMealDetail(data.meals[0]);
+
+    /* fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetail(data.meals[0])) */
+}
+
+displayMealDetail = meal => {
+    console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    mealDetails.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+                <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+            </div>
+    `
+    mealDetails.appendChild(div);
+}
